c5be13dabdbb7447846c66c04b88af6f84babd7a
Redheat <qjyyn@qq.com>
2017-05-31 15:26:45 +0800

    tired package

--- agent/main.go
+++ agent/main.go
@@ -4,14 +4,14 @@ import (
 	//"monitor/agent/jvm"
 	"monitor/agent/config"
 	"fmt"
+	"monitor/agent/conf"
 )
 
 func main() {
-	Config := new(conf.Config)
+	var Config = new(conf.Config)
 	Config.InitConfig("config/agent.conf")
 	fmt.Println(Config.Read("jvm", "jolokiapath"))
 	fmt.Println(Config.Read("jvm", "portstart"))
-	fmt.Printf("%v", Config.Mymap)
 	//jok := &jvm.Jolokia{"/usr/local/jolokia/","jolokia-jvm-1.3.6-agent.jar"}
 	//pid_slice := jvm.GetPid(jok)
 	//jvm.StartJok(jok,pid_slice)
