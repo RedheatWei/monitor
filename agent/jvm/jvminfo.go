f2da5c09eea3dca8d4603cfa0b7bc547bd69f6ea
Redheat <qjyyn@qq.com>
2017-05-31 17:29:42 +0800

    tired package

--- agent/jvm/jvminfo.go
+++ agent/jvm/jvminfo.go
@@ -3,20 +3,39 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"strconv"
+	//"net/url"
 )
-func getJokPort(jok *Jolokia) string{
-	return jok.Portstart
-}
-func GetUrlRes(url string){
+func getUrlRes(url string) (n int,msg []byte){
 	//url := "http://127.0.0.1:"+port+"/jolokia/"
 	response,err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
+		return 1,[]byte{}
 	}else{
 		defer response.Body.Close()
 		body,_ := ioutil.ReadAll(response.Body)
-		fmt.Println(string(body))
-		//return body
+		return 0,body
 	}
-
+}
+func checkPort(port string) int{
+	url := "http://127.0.0.1:"+port+"/jolokia/"
+	code,msg := getUrlRes(url)
+	if code == 0 {
+		return 1
+	}else{
+		fmt.Println(string(msg))
+		return 0
+	}
+}
+func GetPort(jok *Jolokia,pid_slice []string) []string{
+	var port_list []string
+	portstart,_ := strconv.Atoi(jok.Portstart)
+	for port := portstart;port<=portstart+len(pid_slice) ;port++  {
+		strport := strconv.Itoa(port)
+		if checkPort(strport) == 0{
+			port_list = append(port_list, strport)
+		}
+	}
+	return  port_list
 }
--- agent/main.go
+++ agent/main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"monitor/agent/conf"
 	"monitor/agent/jvm"
+	"fmt"
 )
 
 func main() {
-	//jok := &jvm.Jolokia{ReadConfig("jvm","jolokiapath"),ReadConfig("jvm","jolokianame"),ReadConfig("jvm","portstart")}
-	//pid_slice := jvm.GetPid(jok)
+	jok := &jvm.Jolokia{ReadConfig("jvm","jolokiapath"),ReadConfig("jvm","jolokianame"),ReadConfig("jvm","portstart")}
+	pid_slice := jvm.GetPid(jok)
 	//jvm.StartJok(jok,pid_slice)
-	port := "18005"
-	url := "http://127.0.0.1:"+port+"/jolokia/"
-	jvm.GetUrlRes(url)
+	//port := "18005"
+	//url := "http://127.0.0.1:"+port+"/jolokia/"
+	portlist := jvm.GetPort(jok,pid_slice)
+	fmt.Println(portlist)
 }
 func ReadConfig(mod,par string) string{
 	config:= new(conf.Config)
