6963bf048b911e49ba8bc85c838167b6e325941b
Redheat <qjyyn@qq.com>
2017-06-01 15:26:58 +0800

    test json

--- agent/jvm/getapi.go
+++ agent/jvm/getapi.go
@@ -14,12 +14,15 @@ func GetRuntime(portlist []string){
 	for _,port := range portlist{
 		_,res := getJson(port,"read","Threading")
 		js, js_err := simplejson.NewJson(res)
-		fmt.Println(js_err)
-		var nodes = make(map[string]interface{})
-		nodes, _ = js.Map()
-		for key,val := range nodes{
-			fmt.Println(key,val)
+		if js_err != nil{
+			var nodes = make(map[string]interface{})
+			nodes, _ = js.Map()
+			fmt.Println(nodes["request"])
+			//for key,val := range nodes{
+			//	fmt.Println(key,val)
+			//}
 		}
+
 		//fmt.Println(string(res))
 		//dec := json.NewDecoder(strings.NewReader(string(res)))
 		//fmt.Println(dec)
