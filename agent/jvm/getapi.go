3788894c2ba199d412a17aae7791e38d797da99f
Redheat <qjyyn@qq.com>
2017-06-01 13:08:39 +0800

    test json

--- agent/jvm/getapi.go
+++ agent/jvm/getapi.go
@@ -8,12 +8,12 @@ import (
 )
 
 func getJson(port,method,arg string) (n int,res []byte){
-	url := "http://127.0.0.1:"+port+"/jolokia/"+method+"/"+arg
+	url := "http://127.0.0.1:"+port+"/jolokia/"+method+"/java.lang:type="+arg
 	return base.HttpGet(url)
 }
 func GetRuntime(portlist []string){
 	for _,port := range portlist{
-		_,res := getJson(port,"read","java.lang:type=Runtime")
+		_,res := getJson(port,"read","Memory")
 		fmt.Println(string(res))
 		//dec := json.NewDecoder(strings.NewReader(string(res)))
 		//fmt.Println(dec)
