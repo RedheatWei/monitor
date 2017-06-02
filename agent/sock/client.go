1600ac6c611f7e44b17cae6c99b7e05458a1667a
Redheat <qjyyn@qq.com>
2017-06-02 15:12:19 +0800

    add sock

--- agent/sock/client.go
+++ agent/sock/client.go
@@ -23,7 +23,8 @@ func SendMsg(msg string) {
 	}
 	//defer c.Close()
 	//go reader(c)
-	for i:=0;i<len(msg);i+=512{
+	for i:=0;i<len(msg);{
+		i+=512
 		c.Write([]byte(msg[i:i+512]))
 	}
 	//fmt.Println(msg)
