5b31c693aabcbf71fafa6ed198704a9e3705c5ff
Redheat <qjyyn@qq.com>
2017-06-05 11:36:42 +0800

    del sock

--- agent/jvm/getapi.go
+++ agent/jvm/getapi.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	//"monitor/agent/sock"
 	"time"
+	"github.com/bitly/go-simplejson"
 )
 func getResJson(baseUrl []string,method,arg string){
 	for _,url := range baseUrl{
@@ -26,10 +27,19 @@ func AccceptGet(baseUrl []string,method string,args []string){
 		for _,arg := range args{
 			for _,url := range baseUrl {
 				_,res := base.HttpGet(url+method+"/java.lang:type="+arg)
-				fmt.Println(string(res))
+				hanJson(res)
 			}
 		}
 		time.Sleep(time.Duration(Frequency)*time.Second)
 
 	}
+}
+func hanJson(res []byte){
+	js,js_err := simplejson.NewJson(res)
+	if js_err == nil {
+		var nodes= make(map[string]interface{})
+		nodes, _ = js.Map()
+		fmt.Println(nodes)
+	}
+
 }
