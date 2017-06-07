52fa2ba15b7176756b7e9cf55d548bb3ec0d53f8
Redheat <qjyyn@qq.com>
2017-06-07 15:27:07 +0800

    json handle

--- agent/jvm/getapi.go
+++ agent/jvm/getapi.go
@@ -6,23 +6,24 @@ import (
 	"time"
 	"monitor/network"
 	"fmt"
+	"monitor/agent/jvm/jvminfo"
 )
 
 func AccceptGet(baseUrl []string,args []string){
 	for{
-		for _,arg := range args{
+		//for _,arg := range args{
 			for _,url := range baseUrl {
-				_,res := base.HttpGet(url+"read/java.lang:type="+arg)
-				network.UdpSend(base.ReadAgentConfig("default","server"),res)
-				fmt.Println(string(res))
+				fmt.Println(jvminfo.GetMemory(url))
+				//_,res := base.HttpGet(url+"read/java.lang:type="+arg)
+				//network.UdpSend(base.ReadAgentConfig("default","server"),res)
 			}
-		}
+		//}
 		time.Sleep(time.Duration(Frequency)*time.Second)
 
 	}
 }
 
-func resGet(url,arg string) []byte{
+func ResGet(url,arg string) []byte{
 	_,res := base.HttpGet(url+"read/java.lang:type="+arg)
 	return  res
 }
