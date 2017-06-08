e198f298070a28a515946a7edf9309c58ddc7bd1
Redheat <qjyyn@qq.com>
2017-06-08 10:46:13 +0800

    json handle

--- network/server.go
+++ network/server.go
@@ -19,12 +19,12 @@ func StartServer() {
 
 func handleClient(conn *net.UDPConn) {
 	var buf [1024]byte
-	_, _, err := conn.ReadFromUDP(buf[:])
+	n, _, err := conn.ReadFromUDP(buf[:])
 	if err != nil {
 		return
 	}
 	//tmp := buf[:n]
-	handler.ToJson(buf[:])
+	handler.ToJson(buf[:n])
 	//fmt.Println(addr)
 	//fmt.Println(tmp)
 	//fmt.Println(string(buf[:]))
