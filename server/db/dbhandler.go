0aca06279bc9ee7c7cf024d069e8a00a5181fe35
Redheat <qjyyn@qq.com>
2017-06-08 13:04:01 +0800

    json handle

--- server/db/dbhandler.go
+++ server/db/dbhandler.go
@@ -5,34 +5,8 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"fmt"
 	"net"
+	"monitor/base"
 )
-type JsonInfo struct {
-	//class
-	LoadedClassCount int32 `json:"LoadedClassCount"`
-	UnloadedClassCount int32 `json:"UnloadedClassCount"`
-	TotalLoadedClassCount int32 `json:"TotalLoadedClassCount"`
-	//memory
-	HeapMemoryUsageInit int64 `json:"init"`
-	HeapMemoryUsageCommitted int64 `json:"committed"`
-	HeapMemoryUsageMax int64 `json:"max"`
-	HeapMemoryUsageUsed int64 `json:"used"`
-	NonHeapMemoryUsageInit int64 `json:"init"`
-	NonHeapMemoryUsageCommitted int64 `json:"committed"`
-	NonHeapMemoryUsageMax int64 `json:"max"`
-	NonHeapMemoryUsageUsed int64 `json:"used"`
-	//rumtime
-	ClassPath string `json:"ClassPath"`
-	Uptime int64 `json:"Uptime"`
-	//thread
-	TotalStartedThreadCount int32 `json:"TotalStartedThreadCount"`
-	PeakThreadCount int32 `json:"PeakThreadCount"`
-	CurrentThreadCpuTime int64 `json:"CurrentThreadCpuTime"`
-	ThreadCount int32 `json:"ThreadCount"`
-	DaemonThreadCount int32 `json:"DaemonThreadCount"`
-	//base
-	AgentId string `json:"agentId"`
-	TimeStamp int64 `json:"TimeStamp"`
-}
 func connDB() *sql.DB{
 	db, err := sql.Open("mysql", "root:vv231@unix(/var/lib/mysql/mysql.sock)/monitor?charset=utf8")
 	if err != nil{
@@ -40,7 +14,7 @@ func connDB() *sql.DB{
 	}
 	return db
 }
-func InsertDB(js JsonInfo,addr *net.UDPAddr){
+func InsertDB(js base.JsonInfo,addr *net.UDPAddr){
 	db := connDB()
 	stmt, err := db.Prepare("INSERT INTO jvm(LoadedClassCount,UnloadedClassCount,TotalLoadedClassCount,HeapMemoryUsageInit,HeapMemoryUsageCommitted,HeapMemoryUsageMax,HeapMemoryUsageUsed,NonHeapMemoryUsageInit,NonHeapMemoryUsageCommitted,NonHeapMemoryUsageMax,NonHeapMemoryUsageUsed,ClassPath,Uptime,TotalStartedThreadCount,PeakThreadCount,CurrentThreadCpuTime,ThreadCount,DaemonThreadCount,AgentId,TimeStamp,ServerIp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
 	if err != nil{
--- server/handler/rec.go
+++ server/handler/rec.go
@@ -5,36 +5,37 @@ import (
 	"fmt"
 	"monitor/server/db"
 	"net"
+	"monitor/base"
 )
-type JsonInfo struct {
-	//class
-	LoadedClassCount int32 `json:"LoadedClassCount"`
-	UnloadedClassCount int32 `json:"UnloadedClassCount"`
-	TotalLoadedClassCount int32 `json:"TotalLoadedClassCount"`
-	//memory
-	HeapMemoryUsageInit int64 `json:"init"`
-	HeapMemoryUsageCommitted int64 `json:"committed"`
-	HeapMemoryUsageMax int64 `json:"max"`
-	HeapMemoryUsageUsed int64 `json:"used"`
-	NonHeapMemoryUsageInit int64 `json:"init"`
-	NonHeapMemoryUsageCommitted int64 `json:"committed"`
-	NonHeapMemoryUsageMax int64 `json:"max"`
-	NonHeapMemoryUsageUsed int64 `json:"used"`
-	//rumtime
-	ClassPath string `json:"ClassPath"`
-	Uptime int64 `json:"Uptime"`
-	//thread
-	TotalStartedThreadCount int32 `json:"TotalStartedThreadCount"`
-	PeakThreadCount int32 `json:"PeakThreadCount"`
-	CurrentThreadCpuTime int64 `json:"CurrentThreadCpuTime"`
-	ThreadCount int32 `json:"ThreadCount"`
-	DaemonThreadCount int32 `json:"DaemonThreadCount"`
-	//base
-	AgentId string `json:"agentId"`
-	TimeStamp int64 `json:"TimeStamp"`
-}
-func ToJson(rec []byte,addr *net.UDPAddr) JsonInfo{
-	var js JsonInfo
+//type JsonInfo struct {
+//	//class
+//	LoadedClassCount int32 `json:"LoadedClassCount"`
+//	UnloadedClassCount int32 `json:"UnloadedClassCount"`
+//	TotalLoadedClassCount int32 `json:"TotalLoadedClassCount"`
+//	//memory
+//	HeapMemoryUsageInit int64 `json:"init"`
+//	HeapMemoryUsageCommitted int64 `json:"committed"`
+//	HeapMemoryUsageMax int64 `json:"max"`
+//	HeapMemoryUsageUsed int64 `json:"used"`
+//	NonHeapMemoryUsageInit int64 `json:"init"`
+//	NonHeapMemoryUsageCommitted int64 `json:"committed"`
+//	NonHeapMemoryUsageMax int64 `json:"max"`
+//	NonHeapMemoryUsageUsed int64 `json:"used"`
+//	//rumtime
+//	ClassPath string `json:"ClassPath"`
+//	Uptime int64 `json:"Uptime"`
+//	//thread
+//	TotalStartedThreadCount int32 `json:"TotalStartedThreadCount"`
+//	PeakThreadCount int32 `json:"PeakThreadCount"`
+//	CurrentThreadCpuTime int64 `json:"CurrentThreadCpuTime"`
+//	ThreadCount int32 `json:"ThreadCount"`
+//	DaemonThreadCount int32 `json:"DaemonThreadCount"`
+//	//base
+//	AgentId string `json:"agentId"`
+//	TimeStamp int64 `json:"TimeStamp"`
+//}
+func ToJson(rec []byte,addr *net.UDPAddr) base.JsonInfo{
+	var js base.JsonInfo
 	json.Unmarshal(rec,&js)
 	fmt.Println(js.ClassPath)
 	db.InsertDB(js,addr)
