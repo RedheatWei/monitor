7ec68badf27aa8e5e7302310c1362fda35909ddd
Redheat <qjyyn@qq.com>
2017-06-08 13:07:11 +0800

    json handle

--- agent/jvm/getapi.go
+++ agent/jvm/getapi.go
@@ -7,41 +7,14 @@ import (
 	"monitor/network"
 	"monitor/base"
 )
-type jsoninfo struct {
-	//class
-	LoadedClassCount int32 `json:"LoadedClassCount"`
-	UnloadedClassCount int32 `json:"UnloadedClassCount"`
-	TotalLoadedClassCount int32 `json:"TotalLoadedClassCount"`
-	//memory
-	HeapMemoryUsageInit int64 `json:"init"`
-	HeapMemoryUsageCommitted int64 `json:"committed"`
-	HeapMemoryUsageMax int64 `json:"max"`
-	HeapMemoryUsageUsed int64 `json:"used"`
-	NonHeapMemoryUsageInit int64 `json:"init"`
-	NonHeapMemoryUsageCommitted int64 `json:"committed"`
-	NonHeapMemoryUsageMax int64 `json:"max"`
-	NonHeapMemoryUsageUsed int64 `json:"used"`
-	//rumtime
-	ClassPath string `json:"ClassPath"`
-	Uptime int64 `json:"Uptime"`
-	//thread
-	TotalStartedThreadCount int32 `json:"TotalStartedThreadCount"`
-	PeakThreadCount int32 `json:"PeakThreadCount"`
-	CurrentThreadCpuTime int64 `json:"CurrentThreadCpuTime"`
-	ThreadCount int32 `json:"ThreadCount"`
-	DaemonThreadCount int32 `json:"DaemonThreadCount"`
-	//base
-	AgentId string `json:"agentId"`
-	TimeStamp int64 `json:"TimeStamp"`
 
-}
 func dataHandle(url string) []byte{
 	info := jvminfo.GetInfo(url)
 	memory := jvminfo.GetMemory(url)
 	runtime := jvminfo.GetRuntime(url)
 	threading := jvminfo.GetThreading(url)
 	classLoading := jvminfo.GetClassLoading(url)
-	var allinfo jsoninfo
+	var allinfo base.JsonInfo
 	//base
 	allinfo.AgentId = info.Value.Config.AgentId
 	allinfo.TimeStamp = info.TimeStamp
