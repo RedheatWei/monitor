689f8c858ac718fb98e7cbb04791698249e11854
Redheat <qjyyn@qq.com>
2017-06-08 14:35:54 +0800

    data to db

--- server/handler/rec.go
+++ server/handler/rec.go
@@ -4,10 +4,12 @@ import (
 	"encoding/json"
 	"monitor/server/db"
 	"monitor/base"
+	"fmt"
 )
 
 func ToJson(rec []byte,addr string){
 	var js base.JsonInfo
 	json.Unmarshal(rec,&js)
+	fmt.Println(js)
 	go db.InsertDB(js,addr)
 }
