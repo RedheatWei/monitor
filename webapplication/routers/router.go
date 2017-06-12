d42d8054acc94c5a6a9f9e3e7db3cf471d83e76d
Redheat <qjyyn@qq.com>
2017-06-12 12:35:15 +0800

    add web app

--- webapplication/routers/router.go
+++ webapplication/routers/router.go
@@ -7,5 +7,5 @@ import (
 
 func init() {
 	beego.Router("/", &controllers.MainController{})
-	beego.Router("/text", &controllers.TestController{})
+	beego.Router("/test", &controllers.TestController{})
 }
