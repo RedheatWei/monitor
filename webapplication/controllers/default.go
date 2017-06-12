fdfbc35a6f4add8e4a88d7af021763a217911b7e
Redheat <qjyyn@qq.com>
2017-06-12 11:40:56 +0800

    add web app

--- webapplication/controllers/default.go
+++ webapplication/controllers/default.go
@@ -7,9 +7,16 @@ import (
 type MainController struct {
 	beego.Controller
 }
-
+type TestController struct {
+	beego.Controller
+}
 func (c *MainController) Get() {
 	c.Data["Website"] = "golang.org"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.html"
+}
+
+func (c *TestController) Get(){
+	c.Data["test"] = "this is a test"
+	c.TplName = "test.html"
 }
