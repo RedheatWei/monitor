9c71cc6b5f39697761e46d563b938a9d19c6ce68
Redheat <qjyyn@qq.com>
2017-06-14 11:20:16 +0800

    fix bug of java path

--- server/handler/rec.go
+++ server/handler/rec.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"monitor/base"
 	"monitor/server/db"
+	"fmt"
 )
 
 func ToJson(rec []byte,addr string){
 	var js base.Senddata
 	json.Unmarshal(rec,&js)
 	//fmt.Println()
+	fmt.Println(string(js))
 	go db.InsertDB(js.JvmData,addr)
 }
