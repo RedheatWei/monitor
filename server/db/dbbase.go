0f49a98bcedd166a6be8943998ef9a1b73b1150e
Redheat <qjyyn@qq.com>
2017-06-15 14:48:37 +0800

    handle database layout

--- server/db/dbbase.go
+++ server/db/dbbase.go
@@ -2,12 +2,18 @@ package db
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/xormplus/xorm"
-	"monitor/network"
 	"fmt"
+	"monitor/base"
 )
 var engine *xorm.Engine
+
+var ServerConfig base.ServerConfig
+func init()  {
+	ServerConfig = base.GetConfig()
+}
+
 func ConnDB() *xorm.Engine{
-	dbconf := network.ServerConfig.DB
+	dbconf := ServerConfig.DB
 	dbconn := fmt.Sprintf("%s:%s@%s(%s)/%s?charset=%s",dbconf.User,dbconf.Passwd,dbconf.Host,dbconf.Protocol,dbconf.Database,dbconf.Charset)
 	var err error
 	engine, err = xorm.NewEngine(dbconf.Type, dbconn)
