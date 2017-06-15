9afa7d05f22034343200262e6f93c269b41ea97b
Redheat <qjyyn@qq.com>
2017-06-15 09:09:38 +0800

    add type for system info

--- agent/system/getapi.go
+++ agent/system/getapi.go
@@ -11,6 +11,7 @@ import (
 	"monitor/base"
 	"monitor/network"
 	"time"
+	"fmt"
 	"encoding/json"
 )
 
@@ -87,6 +88,7 @@ func CollectSysInfo(){
 		case "SysNetInfo":
 			data.SysNetInfo = CollectSysNetInfo()
 		}
+		fmt.Pringln(data)
 		i,_ := json.Marshal(data)
 		network.UdpSend(base.ReadAgentConfig("default","server"),i)
 		time.Sleep(time.Duration(Frequency)*time.Second)
