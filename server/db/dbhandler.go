2afbe97e623aed921dc2661317cbe80306495d65
Redheat <qjyyn@qq.com>
2017-06-15 10:50:15 +0800

    add type for system info

--- server/db/dbhandler.go
+++ server/db/dbhandler.go
@@ -4,31 +4,12 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"fmt"
-	"monitor/base"
 )
 
-
-func connDB() *sql.DB{
-	db, err := sql.Open("mysql", "root:vv231@unix(/var/lib/mysql/mysql.sock)/monitor?charset=utf8")
+func ConnDB() *sql.DB{
+	db, err := sql.Open("mysql", "monitor:monitor@tcp(127.0.0.1)/monitor?charset=utf8")
 	if err != nil{
 		fmt.Println(err)
 	}
 	return db
-}
-func InsertJvmDB(js base.JvmInfo,addr string){
-	db := connDB()
-	defer db.Close()
-	stmt, err := db.Prepare("INSERT INTO collect_jvm(LoadedClassCount,UnloadedClassCount,TotalLoadedClassCount,HeapMemoryUsageInit,HeapMemoryUsageCommitted,HeapMemoryUsageMax,HeapMemoryUsageUsed,NonHeapMemoryUsageInit,NonHeapMemoryUsageCommitted,NonHeapMemoryUsageMax,NonHeapMemoryUsageUsed,ClassPath,Uptime,TotalStartedThreadCount,PeakThreadCount,CurrentThreadCpuTime,ThreadCount,DaemonThreadCount,AgentId,TimeStamp,ServerIp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
-	if err != nil{
-		fmt.Println(err)
-	}
-	res, err := stmt.Exec(js.LoadedClassCount,js.UnloadedClassCount,js.TotalLoadedClassCount,js.HeapMemoryUsageInit,js.HeapMemoryUsageCommitted,js.HeapMemoryUsageMax,js.HeapMemoryUsageUsed,js.NonHeapMemoryUsageInit,js.NonHeapMemoryUsageCommitted,js.NonHeapMemoryUsageMax,js.NonHeapMemoryUsageUsed,js.ClassPath,js.Uptime,js.TotalStartedThreadCount,js.PeakThreadCount,js.CurrentThreadCpuTime,js.ThreadCount,js.DaemonThreadCount,js.AgentId,js.TimeStamp,addr)
-	if err != nil{
-		fmt.Println(err)
-	}
-	id,err := res.LastInsertId()
-	if err!=  nil{
-		fmt.Println(err)
-	}
-	fmt.Println(id)
 }
--- server/handler/rec.go
+++ server/handler/rec.go
@@ -2,29 +2,47 @@ package handler
 
 import (
 	"encoding/json"
-	//"monitor/base"
-	//"monitor/server/db"
-	"fmt"
 	"github.com/bitly/go-simplejson"
 	"monitor/server/db"
 	"monitor/base"
 )
-
 func ToJson(rec []byte,addr string){
 	//json.Unmarshal(rec,&js)
 	js, _ := simplejson.NewJson(rec)
-	fmt.Println(js)
 	js_map,_ := js.Map()
 	switch js_map["Type"] {
 	case "JvmInfo":
-		var Data base.JvmInfo
-		json.Unmarshal(rec,&Data)
-		fmt.Println(Data)
-		go db.InsertJvmDB(Data,addr)
+		var info base.JvmInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertJvmDB(info,addr)
 	case "jvminfo":
 		var Data base.JvmInfo
 		json.Unmarshal(rec,&Data)
-		fmt.Println(Data)
 		go db.InsertJvmDB(Data,addr)
+	case "SysMemInfo":
+		var info base.SysMemInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertMemDB(info,addr)
+	case "SysCpuInfo":
+		var info base.SysCpuInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertCpuDB(info,addr)
+	case "SysDiskInfo":
+		var info base.SysDiskInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertDiskDB(info,addr)
+	case "SysHostInfo":
+		var info base.SysHostInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertHostDB(info,addr)
+	case "SysLoadInfo":
+		var info base.SysLoadInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertLoadDB(info,addr)
+	case "SysNetInfo":
+		var info base.SysNetInfo
+		json.Unmarshal(rec,&info)
+		go db.InsertNetDB(info,addr)
+
 	}
 }
