db9640ab2c4e6ffce4fa36ec44303959f46b940c
Redheat <qjyyn@qq.com>
2017-06-15 10:40:25 +0800

    add type for system info

--- server/handler/rec.go
+++ server/handler/rec.go
@@ -1,24 +1,30 @@
 package handler
 
 import (
-	//"encoding/json"
+	"encoding/json"
 	//"monitor/base"
 	//"monitor/server/db"
 	"fmt"
 	"github.com/bitly/go-simplejson"
+	"monitor/server/db"
+	"monitor/base"
 )
 
 func ToJson(rec []byte,addr string){
-	//var js interface{}
 	//json.Unmarshal(rec,&js)
 	js, _ := simplejson.NewJson(rec)
 	fmt.Println(js)
-	x,_ := js.Map()
-	fmt.Println(x["Type"])
-	//switch js.Type {
-	//case "JvmInfo":
-	//	go db.InsertJvmDB(js.Data,addr)
-	//case "jvminfo":
-	//	go db.InsertJvmDB(js.JvmInfo,addr)
-	//}
+	js_map,_ := js.Map()
+	switch js_map["Type"] {
+	case "JvmInfo":
+		var Data base.JvmInfo
+		json.Unmarshal(rec,&Data)
+		fmt.Println(Data)
+		go db.InsertJvmDB(Data,addr)
+	case "jvminfo":
+		var Data base.JvmInfo
+		json.Unmarshal(rec,&Data)
+		fmt.Println(Data)
+		go db.InsertJvmDB(Data,addr)
+	}
 }
