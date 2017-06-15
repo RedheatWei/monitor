683280a7bd8cd2f853b94873ce0f41a38f358cc2
Redheat <qjyyn@qq.com>
2017-06-15 10:51:21 +0800

    add type for system info

---
+++ server/db/jvmdb.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"monitor/base"
+	"fmt"
+)
+
+func InsertJvmDB(js base.JvmInfo,addr string){
+	db := ConnDB()
+	defer db.Close()
+	stmt, err := db.Prepare("INSERT INTO collect_jvm(LoadedClassCount,UnloadedClassCount,TotalLoadedClassCount,HeapMemoryUsageInit,HeapMemoryUsageCommitted,HeapMemoryUsageMax,HeapMemoryUsageUsed,NonHeapMemoryUsageInit,NonHeapMemoryUsageCommitted,NonHeapMemoryUsageMax,NonHeapMemoryUsageUsed,ClassPath,Uptime,TotalStartedThreadCount,PeakThreadCount,CurrentThreadCpuTime,ThreadCount,DaemonThreadCount,AgentId,TimeStamp,ServerIp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)")
+	if err != nil{
+		fmt.Println(err)
+	}
+	res, err := stmt.Exec(js.LoadedClassCount,js.UnloadedClassCount,js.TotalLoadedClassCount,js.HeapMemoryUsageInit,js.HeapMemoryUsageCommitted,js.HeapMemoryUsageMax,js.HeapMemoryUsageUsed,js.NonHeapMemoryUsageInit,js.NonHeapMemoryUsageCommitted,js.NonHeapMemoryUsageMax,js.NonHeapMemoryUsageUsed,js.ClassPath,js.Uptime,js.TotalStartedThreadCount,js.PeakThreadCount,js.CurrentThreadCpuTime,js.ThreadCount,js.DaemonThreadCount,js.AgentId,js.TimeStamp,addr)
+	if err != nil{
+		fmt.Println(err)
+	}
+	id,err := res.LastInsertId()
+	if err!=  nil{
+		fmt.Println(err)
+	}
+	fmt.Println(id)
+}
