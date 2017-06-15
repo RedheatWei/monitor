38f3cbe656b8f0910c92ab92f7d457e44f9d1eb5
Redheat <qjyyn@qq.com>
2017-06-15 17:08:53 +0800

    iplist

--- server/db/jvm/column.go
+++ server/db/jvm/column.go
@@ -1,27 +1,27 @@
 package jvm
 
 type Collect_jvm struct {
-	Id int64 `xorm:"autoincr notnull unique"`
-	LoadedClassCount      int32 `xorm:"notnull"`
-	UnloadedClassCount    int32 `xorm:"notnull"`
-	TotalLoadedClassCount int32 `xorm:"notnull"`
-	HeapMemoryUsageInit      int64 `xorm:"notnull"`
-	HeapMemoryUsageCommitted int64 `xorm:"notnull"`
-	HeapMemoryUsageMax       int64 `xorm:"notnull"`
-	HeapMemoryUsageUsed      int64 `xorm:"notnull"`
-	NonHeapMemoryUsageInit      int64 `xorm:"notnull"`
-	NonHeapMemoryUsageCommitted int64 `xorm:"notnull"`
-	NonHeapMemoryUsageMax       int64 `xorm:"notnull"`
-	NonHeapMemoryUsageUsed      int64 `xorm:"notnull"`
-	ClassPath string `xorm:"notnull"`
-	Uptime    int64 `xorm:"notnull"`
-	TotalStartedThreadCount int32 `xorm:"notnull"`
-	PeakThreadCount         int32 `xorm:"notnull"`
-	CurrentThreadCpuTime    int64 `xorm:"notnull"`
-	ThreadCount             int32 `xorm:"notnull"`
-	DaemonThreadCount       int32 `xorm:"notnull"`
-	AgentId   string `xorm:"notnull"`
-	TimeStamp int64 `xorm:"notnull"`
-	CreateTime int64 `xorm:"notnull created"`
-	ServerIp string `xorm:"notnull"`
+	Id int64 `xorm:"autoincr notnull unique 'id'"`
+	LoadedClassCount      int32 `xorm:"notnull 'LoadedClassCount'"`
+	UnloadedClassCount    int32 `xorm:"notnull 'UnloadedClassCount'"`
+	TotalLoadedClassCount int32 `xorm:"notnull 'TotalLoadedClassCount'"`
+	HeapMemoryUsageInit      int64 `xorm:"notnull 'HeapMemoryUsageInit'"`
+	HeapMemoryUsageCommitted int64 `xorm:"notnull 'HeapMemoryUsageCommitted'"`
+	HeapMemoryUsageMax       int64 `xorm:"notnull 'HeapMemoryUsageMax'"`
+	HeapMemoryUsageUsed      int64 `xorm:"notnull 'HeapMemoryUsageUsed'"`
+	NonHeapMemoryUsageInit      int64 `xorm:"notnull 'NonHeapMemoryUsageInit'"`
+	NonHeapMemoryUsageCommitted int64 `xorm:"notnull 'NonHeapMemoryUsageCommitted'"`
+	NonHeapMemoryUsageMax       int64 `xorm:"notnull 'NonHeapMemoryUsageMax'"`
+	NonHeapMemoryUsageUsed      int64 `xorm:"notnull 'NonHeapMemoryUsageUsed'"`
+	ClassPath string `xorm:"notnull 'ClassPath'"`
+	Uptime    int64 `xorm:"notnull 'Uptime'"`
+	TotalStartedThreadCount int32 `xorm:"notnull 'TotalStartedThreadCount'"`
+	PeakThreadCount         int32 `xorm:"notnull 'PeakThreadCount'"`
+	CurrentThreadCpuTime    int64 `xorm:"notnull 'CurrentThreadCpuTime'"`
+	ThreadCount             int32 `xorm:"notnull 'ThreadCount'"`
+	DaemonThreadCount       int32 `xorm:"notnull 'DaemonThreadCount'"`
+	AgentId   string `xorm:"notnull 'AgentId'"`
+	TimeStamp int64 `xorm:"notnull 'TimeStamp'"`
+	CreateTime int64 `xorm:"notnull created 'CreateTime'"`
+	ServerIp string `xorm:"notnull 'ServerIp'"`
 }
