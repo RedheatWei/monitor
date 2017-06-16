9b5d08b4f8a2b6e8635fef4f599d34c1e04fbe9b
Redheat <qjyyn@qq.com>
2017-06-16 13:39:06 +0800

    db handler

--- server/db/mem/column.go
+++ server/db/mem/column.go
@@ -1,3 +1,31 @@
 package mem
 
-
+type Collect_mem struct {
+	Id int64 `xorm:"autoincr notnull unique 'id'"`
+	ServerId      int64 `xorm:"notnull 'Serverid'"`
+	VirtualMemoryStattotal uint64  `xorm:"notnull 'VirtualMemoryStattotal'"`
+	VirtualMemoryStatavailable uint64 `xorm:"notnull 'VirtualMemoryStatavailable'"`
+	VirtualMemoryStatused uint64 `xorm:"notnull 'VirtualMemoryStatused'"`
+	VirtualMemoryStatusedPercent float64 `xorm:"notnull 'VirtualMemoryStatusedPercent'"`
+	VirtualMemoryStatfree uint64 `xorm:"notnull 'VirtualMemoryStatfree'"`
+	VirtualMemoryStatactive uint64 `xorm:"notnull 'VirtualMemoryStatactive'"`
+	VirtualMemoryStatinactive uint64 `xorm:"notnull 'VirtualMemoryStatinactive'"`
+	VirtualMemoryStatwired uint64 `xorm:"notnull 'VirtualMemoryStatwired'"`
+	VirtualMemoryStatbuffers uint64 `xorm:"notnull 'VirtualMemoryStatbuffers'"`
+	VirtualMemoryStatcached uint64 `xorm:"notnull 'VirtualMemoryStatcached'"`
+	VirtualMemoryStatwriteback uint64 `xorm:"notnull 'VirtualMemoryStatwriteback'"`
+	VirtualMemoryStatdirty uint64 `xorm:"notnull 'VirtualMemoryStatdirty'"`
+	VirtualMemoryStatwritebacktmp uint64 `xorm:"notnull 'VirtualMemoryStatwritebacktmp'"`
+	VirtualMemoryStatshared uint64 `xorm:"notnull 'VirtualMemoryStatshared'"`
+	VirtualMemoryStatslab uint64 `xorm:"notnull 'VirtualMemoryStatslab'"`
+	VirtualMemoryStatpagetables uint64 `xorm:"notnull 'VirtualMemoryStatpagetables'"`
+	VirtualMemoryStatswapcached uint64 `xorm:"notnull 'VirtualMemoryStatswapcached'"`
+	SwapMemoryStattotal uint64 `xorm:"notnull 'SwapMemoryStattotal'"`
+	SwapMemoryStatused uint64  `xorm:"notnull 'SwapMemoryStatused'"`
+	SwapMemoryStatfree uint64 `xorm:"notnull 'SwapMemoryStatfree'"`
+	SwapMemoryStatusedPercent float64 `xorm:"notnull 'SwapMemoryStatusedPercent'"`
+	SwapMemoryStatsin uint64 `xorm:"notnull 'SwapMemoryStatsin'"`
+	SwapMemoryStatsout uint64 `xorm:"notnull 'SwapMemoryStatsout'"`
+	TimeStamp      int64 `xorm:"notnull 'TimeStamp'"`
+	CreateTime int64 `xorm:"notnull created 'CreateTime'"`
+}
--- server/db/mem/memdb.go
+++ server/db/mem/memdb.go
@@ -3,8 +3,40 @@ package mem
 import (
 	"monitor/server/base"
 	"fmt"
+	"monitor/server/db"
 )
 
 func InsertMemDB(js base.SysMemInfo,serverid int64){
-	fmt.Println(js)
+	db := db.ConnDB()
+	mem := new(Collect_mem)
+	mem.ServerId = serverid
+	mem.VirtualMemoryStattotal = js.VirtualMemoryStat.Total
+	mem.VirtualMemoryStatavailable = js.VirtualMemoryStat.Available
+	mem.VirtualMemoryStatused = js.VirtualMemoryStat.Used
+	mem.VirtualMemoryStatusedPercent = js.VirtualMemoryStat.UsedPercent
+	mem.VirtualMemoryStatfree = js.VirtualMemoryStat.Free
+	mem.VirtualMemoryStatactive = js.VirtualMemoryStat.Active
+	mem.VirtualMemoryStatinactive = js.VirtualMemoryStat.Inactive
+	mem.VirtualMemoryStatwired = js.VirtualMemoryStat.Wired
+	mem.VirtualMemoryStatbuffers = js.VirtualMemoryStat.Buffers
+	mem.VirtualMemoryStatcached = js.VirtualMemoryStat.Cached
+	mem.VirtualMemoryStatwriteback = js.VirtualMemoryStat.Writeback
+	mem.VirtualMemoryStatdirty = js.VirtualMemoryStat.Dirty
+	mem.VirtualMemoryStatwritebacktmp = js.VirtualMemoryStat.WritebackTmp
+	mem.VirtualMemoryStatshared = js.VirtualMemoryStat.Shared
+	mem.VirtualMemoryStatslab = js.VirtualMemoryStat.Slab
+	mem.VirtualMemoryStatpagetables = js.VirtualMemoryStat.PageTables
+	mem.VirtualMemoryStatswapcached = js.VirtualMemoryStat.SwapCached
+	mem.SwapMemoryStattotal = js.SwapMemoryStat.Total
+	mem.SwapMemoryStatused = js.SwapMemoryStat.Used
+	mem.SwapMemoryStatfree = js.SwapMemoryStat.Free
+	mem.SwapMemoryStatusedPercent = js.SwapMemoryStat.UsedPercent
+	mem.SwapMemoryStatsin = js.SwapMemoryStat.Sin
+	mem.SwapMemoryStatsout = js.SwapMemoryStat.Sout
+	mem.TimeStamp = js.TimeStamp
+	affected, err := db.Insert(mem)
+	if err != nil{
+		fmt.Println(err)
+	}
+	fmt.Println(affected)
 }
