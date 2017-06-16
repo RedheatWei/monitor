cd1016c879db48b00f33c7514aea6545250c1d91
Redheat <qjyyn@qq.com>
2017-06-16 10:58:43 +0800

    iplist

---
+++ server/db/info/column.go
@@ -0,0 +1,14 @@
+package info
+
+type Server_info_ip struct {
+	Id int64 `xorm:"autoincr notnull unique 'id'"`
+	ServerId int32 `xorm:"notnull 'ServerId'"`
+	Ip string `xorm:"notnull 'Ip'"`
+	Type string `xorm:"notnull 'Type'"`
+	CreateTime int64 `xorm:"notnull created 'CreateTime'"`
+}
+type Server_info_base struct {
+	Id int64 `xorm:"autoincr notnull unique 'id'"`
+	Server string `xorm:"notnull 'Server'"`
+	CreateTime int64 `xorm:"notnull created 'CreateTime'"`
+}
