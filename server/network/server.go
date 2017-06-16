ab4903c7f56bac6a389ff2b90c27fd902fdd9650
Redheat <qjyyn@qq.com>
2017-06-16 09:24:26 +0800

    iplist

--- server/network/server.go
+++ server/network/server.go
@@ -33,6 +33,7 @@ func StartServer() {
 //处理客户端的消息
 func handleClient(conn *net.UDPConn) {
 	var buf [1024]byte
+	fmt.Println(100)
 	n, addr, err := conn.ReadFromUDP(buf[:])
 	if err != nil {
 		fmt.Println(err)
