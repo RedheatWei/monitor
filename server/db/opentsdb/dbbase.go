0312dac81d54f0c10602f63a725cef0a3479c555
Redheat <qjyyn@qq.com>
2017-07-12 16:37:57 +0800

    add opentsdb

--- server/db/opentsdb/dbbase.go
+++ server/db/opentsdb/dbbase.go
@@ -42,7 +42,6 @@ func httpGet(url string) ([]byte,error){
 }
 
 func HttpPost(url,data string) ([]byte,error){
-	fmt.Println(url)
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", url, strings.NewReader(data))
 	if err != nil {
