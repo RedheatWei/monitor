b6cbe9e3049204e149f31ffa1c1fdaeda7bcfc54
Redheat <qjyyn@qq.com>
2017-07-12 16:22:56 +0800

    add opentsdb

--- server/db/opentsdb/load/load.go
+++ server/db/opentsdb/load/load.go
@@ -28,5 +28,7 @@ func InsertLoadDB(js base.SysLoadInfo,server string){
 	if err!=nil{
 		fmt.Println(err)
 	}
-	opentsdb.SendToTsDb(string(b))
+	var load_js Collect_load
+	json.Unmarshal(b,&load_js)
+	opentsdb.SendToTsDb(string(load_js))
 }
