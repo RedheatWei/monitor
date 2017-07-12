eaa2a8b6d063703c7ddf7d8811157e433619e18d
Redheat <qjyyn@qq.com>
2017-07-12 16:16:42 +0800

    add opentsdb

--- server/network/server.go
+++ server/network/server.go
@@ -51,7 +51,6 @@ func handleClient(conn *net.UDPConn) {
 }
 //检查ip是否通行
 func checkIp(ip string) (bool,int64,string){
-	fmt.Println(ip)
 	var is_in = bool(false)
 	var serverid int64
 	var server string
