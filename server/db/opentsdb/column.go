4ab303d2e58299d5276ba0132046ecbfa9e6821e
Redheat <qjyyn@qq.com>
2017-07-14 10:55:28 +0800

    add opentsdb

--- server/db/opentsdb/column.go
+++ server/db/opentsdb/column.go
@@ -11,3 +11,15 @@ type Tags struct {
 	CtimeStamp int64 `json:"ctime_stamp"`
 	Ip string `json:"ip"`
 }
+type Collect_disk struct {
+	Metric string `json:"metric"`
+	TimeStamp int64 `json:"timestamp"`
+	Value float64 `json:"value"`
+	Tags Tags_disk `json:"tags"`
+}
+type Tags_disk struct {
+	Server string `json:"server"`
+	CtimeStamp int64 `json:"ctime_stamp"`
+	Ip string `json:"ip"`
+	UsageStatPath string `json:"usage_stat_path"`
+}
